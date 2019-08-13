8c6c2ff206b76c9b39c866082e3299172322ed5f
justinma98 <justinma3@gmail.com>
2019-08-12 23:09:40 -0700

    toolbar for webpages

---
+++ src/components/Toolbar/Toolbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Toolbar.scss";
+
+const toolbar = props => {
+  return (
+    <div className="toolbar">
+      <div className="toolbar__button -back" />
+      <div className="toolbar__button -forward" />
+      <div className="toolbar__button -refresh" />
+      <div className="toolbar__search">
+        <div className="toolbar__url" contentEditable spellcheck="false">{props.url}</div>
+      </div>
+    </div>
+  );
+};
+
+export default toolbar;
--- src/content/desktopContent.js
+++ src/content/desktopContent.js
@@ -2,6 +2,7 @@ import React from "react";
 import IconContainer from "../components/IconContainer/IconContainer";
 import InfoCard from "../components/InfoCard/InfoCard";
 import Typer from "../components/Typer/Typer";
+import Toolbar from "../components/Toolbar/Toolbar";
 import { FILE_TYPE } from "../constants";
 import DEV from "./devContent";
 import ME from "./meContent";
@@ -42,6 +43,7 @@ const DESKTOP = [
     type: FILE_TYPE.TEXT,
     content: (
       <div className={FILE_TYPE.TEXT}>
+        <Toolbar url="localhost:3000"/>
         <h3>Dev Projects</h3>
         <InfoCard src="https://www.w3schools.com/w3css/img_lights.jpg" title="This is title" description="This is a description. This is a description. This is a description."/>
         <InfoCard src="https://www.w3schools.com/w3css/img_lights.jpg" title="This is another title" description="This description. d is a deddddddddscription. This is a description."/>
